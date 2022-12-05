1486b82cffd881770d55469e93bb4bc24c6e5f12
tream2001 <45127883+tream2001@users.noreply.github.com>
2022-12-05 18:05:58 -0500

    Fixed Encoder's pid output

--- source/usb_tx.sv
+++ source/usb_tx.sv
@@ -5,6 +5,8 @@
 // Version:         1.0 Initial Design Entry
 // Description:     USB TX Module of USB Full-Speed Bulk-Transfer Endpoint AHB-Lite SoC Module
 
+//TODO: CRC
+
 module usb_tx
 (
     input wire clk, n_rst, TX_Start,
@@ -18,21 +20,78 @@ module usb_tx
     localparam [1:0] TX_PACKET_NAK = 2'd2;
     localparam [1:0] TX_PACKET_STALL = 2'd3;
 
-    // Control FSM
-    localparam [3:0]
+    // Control FSM Variables
+    localparam [2:0]
         idle = 4'd0,
-        eidle = 4'd1,
-        sync = 4'd2,
-        pid = 4'd3,
-        data = 4'd4,
-        crc1 = 4'd5,
-        crc2 = 4'd6,
-        eof1 = 4'd7,
-        eof2 = 4'd8;
-
+        sync = 4'd1,
+        pid = 4'd2,
+        data = 4'd3,
+        crc1 = 4'd4,
+        crc2 = 4'd5,
+        eof1 = 4'd6,
+        eof2 = 4'd7;
     reg [2:0] state, nxt_state;
     reg syncEn, pidEn, dataEn, crcEn, eofEn; // May want to just use the current state itself rather than these enable signals
 
+    // Clock Divider Variables
+    reg shiftEn;
+    localparam [1:0]
+        firstCycle = 2'd0,
+        secondCycle = 2'd1,
+        thirdCycle = 2'd2;
+    reg [1:0] cycleState, nxt_cycleState;
+    reg [3:0] rollover;
+
+    // Bit Counter Variables
+    reg endByte;
+    reg [2:0] bitNum, nxt_bitNum;
+
+    // CRC-16 Variables
+    reg crcBit; // Output by a pts shift register filled with CRC subcircuit
+
+    // Bit-Stuffer Variables
+    reg stuffEn;
+    reg [2:0] numOne, nxt_numOne;
+
+    // Shift Register Variables
+    reg nxtBit;
+
+    // Encoder Variables
+    reg nxt_Dplus_Out, nxt_Dminus_Out;
+
+    // Clock Divider
+    flex_counter #(
+        .NUM_CNT_BITS(4)
+    )
+    CYCLE_COUNTER (
+        .clk(clk),
+        .n_rst(clk),
+        .clear(1'b0),
+        .count_enable(1'b1),
+        .rollover_val(rollover),
+        .rollover_flag(shiftEn)
+    );
+
+    always_comb begin
+        if (shiftEn == 1'b1) begin
+            case(cycleState)
+                firstCycle: nxt_cycleState = secondCycle;
+                secondCycle: nxt_cycleState = thirdCycle;
+                thirdCycle: nxt_cycleState = firstCycle;
+            endcase
+        end
+
+        case(cycleState)
+            firstCycle, secondCycle: rollover = 3'd8;
+            thirdCycle: rollover = 3'd9;
+            default: rollover = 3'd8;
+        endcase
+    end
+
+    // CRC-16
+    //TODO
+
+    // Control FSM
     always_ff @(negedge n_rst, posedge clk) begin
         if (!n_rst)
             state <= idle;
@@ -47,18 +106,12 @@ module usb_tx
             idle: begin
                 if (TX_Start == 1'b1) nxt_state = sync;
             end
-            eidle: begin
-                if (TX_Start == 1'b1) nxt_state = sync;
-            end
             sync: begin
                 if (endByte == 1'b1) nxt_state = pid;
             end
             pid: begin
                 if (endByte == 1'b1) begin
-                    if (TX_Packet == TX_PACKET_DATA0) begin
-                        if (Buffer_Occupancy == 7'd0) nxt_state = eidle;
-                        else nxt_state = data;
-                    end
+                    if (TX_Packet == TX_PACKET_DATA0)  nxt_state = data;
                     else nxt_state = eof1;
                 end
             end
@@ -91,9 +144,6 @@ module usb_tx
 
         case(state)
             // idle: do nothing
-            eidle: begin
-                TX_Error = 1'b1;
-            end
             sync: begin
                 TX_Transfer_Active = 1'b1;
                 syncEn = 1'b1;
@@ -126,9 +176,6 @@ module usb_tx
     end
 
     // Bit Counter
-    reg endByte;
-    reg [2:0] bitNum, nxt_bitNum;
-
     always_ff @(negedge n_rst, posedge clk) begin
         if (!n_rst) begin
             bitNum <= 3'd0;
@@ -156,13 +203,7 @@ module usb_tx
         end
     end
 
-    // Clock Divider
-    reg shiftEn;
-    //TODO
-
     // Shift Register
-    reg nxtBit;
-
     flex_pts_sr #(
         .NUM_BITS(8),
         .SHIFT_MSB(1'b0) // Make sure endianness is correct
@@ -177,9 +218,6 @@ module usb_tx
     );
 
     // Bit-Stuffer
-    reg enStuff;
-    reg [2:0] numOne, nxt_numOne;
-
     always_ff @(negedge n_rst, posedge clk) begin
         if (!n_rst)
             numOne <= 3'd0;
@@ -190,9 +228,9 @@ module usb_tx
     always_comb begin
         nxt_numOne = numOne;
 
-        if shiftEn begin
-            if dataEn begin
-                if nxtBit begin
+        if (shiftEn) begin
+            if (dataEn) begin
+                if (nxtBit) begin
                     if (numOne == 3'd5) nxt_numOne = 3'd0;
                     else nxt_numOne = numOne + 1;
                 end
@@ -210,19 +248,17 @@ module usb_tx
     end
 
     always_comb begin
-        enStuff = 1'b0;
+        stuffEn = 1'b0;
 
         if ((numOne == 3'd5) && (dataEn == 1'b1) && (nxtBit == 1'b1)) begin
-            enStuff = 1'b1;
+            stuffEn = 1'b1;
         end
         else if ((numOne == 3'd5) && (crcEn == 1'b1) && (crcBit == 1'b1)) begin
-            enStuff = 1'b1;
+            stuffEn = 1'b1;
         end
     end
 
     // Encoder
-    reg nxt_Dplus_Out, nxt_Dminus_Out;
-
     always_ff @(negedge n_rst, posedge clk) begin
         if (!n_rst)
             Dplus_Out <= 1'b1;
@@ -250,46 +286,54 @@ module usb_tx
                 endcase
             end
             else if (pidEn == 1'b1) begin
-                case(TX_Packet)
+                case (TX_Packet)
                     TX_PACKET_DATA0: begin // 11000011
-                        // 1
-                        3'd0, 3'd1, 3'd6, 3'd7: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
-                        end
-                        // 0
-                        3'd2, 3'd3, 3'd4, 3'd5: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
-                        end
+                        case(bitNum)
+                            // 1
+                            3'd0, 3'd1, 3'd6, 3'd7: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
+                            end
+                            // 0
+                            3'd2, 3'd3, 3'd4, 3'd5: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
+                            end
+                        endcase
                     end
                     TX_PACKET_ACK: begin // 01001011
-                        // 1
-                        3'd1, 3'd4, 3'd6, 3'd7: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
-                        end
-                        // 0
-                        3'd0, 3'd2, 3'd3, 3'd5: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
-                        end
+                        case(bitNum)
+                            // 1
+                            3'd1, 3'd4, 3'd6, 3'd7: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
+                            end
+                            // 0
+                            3'd0, 3'd2, 3'd3, 3'd5: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
+                            end
+                        endcase
                     end
                     TX_PACKET_NAK: begin // 01011010
-                        // 1
-                        3'd1, 3'd3, 3'd4, 3'd6: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
-                        end
-                        // 0
-                        3'd0, 3'd2, 3'd5, 3'd7: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
-                        end
+                        case(bitNum)
+                            // 1
+                            3'd1, 3'd3, 3'd4, 3'd6: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
+                            end
+                            // 0
+                            3'd0, 3'd2, 3'd5, 3'd7: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
+                            end
+                        endcase
                     end
                     TX_PACKET_STALL: begin // 01111000
-                        // 1
-                        3'd1, 3'd2, 3'd3, 3'd4: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
-                        end
-                        // 0
-                        3'd0, 3'd5, 3'd6, 3'd7: begin
-                            {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
-                        end
+                        case(bitNum)
+                            // 1
+                            3'd1, 3'd2, 3'd3, 3'd4: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = {Dplus_Out, Dminus_Out};
+                            end
+                            // 0
+                            3'd0, 3'd5, 3'd6, 3'd7: begin
+                                {nxt_Dplus_Out, nxt_Dminus_Out} = ~{Dplus_Out, Dminus_Out};
+                            end
+                        endcase
                     end
                 endcase
             end
