3085c6625ba4fa7d33c0364fc4f61025345923c1
Robert Sammelson <robert@sammelson.com>
2022-12-07 11:50:42 -0500

    More progress on rx testbench

--- test-benches/tb_usb_rx.sv
+++ test-benches/tb_usb_rx.sv
@@ -15,99 +15,98 @@ localparam [3:0] NAK_PID    = 4'b1010;
 localparam [3:0] STALL_PID  = 4'b1110;
 
 // Test Bench DUT Port Signals
-reg tb_clk, tb_n_rst;
-reg tb_dp, tb_dm;
-wire [2:0] tb_rx_packet;
-wire tb_rx_data_ready, tb_rx_transfer_active, tb_rx_error;
-wire tb_flush, tb_store_rx_packet_data;
-wire [7:0] tb_rx_packet_data;
-reg [6:0] tb_buffer_occupancy;
+reg clk, n_rst;
+// USB input lines
+reg dp, dm;
+// Outputs to AHB-lite interface
+wire [2:0] rx_packet;
+wire rx_data_ready, rx_transfer_active, rx_error;
+// I/O from FIFO
+wire flush, store_rx_packet_data;
+wire [7:0] rx_packet_data;
+reg [6:0] buffer_occupancy;
 
 // Test Bench Expected Signals
-reg [2:0] tb_expected_rx_packet;
-reg tb_expected_rx_data_ready;
-reg tb_expected_rx_transfer_active;
-reg tb_expected_rx_error;
-reg tb_expected_flush;
-reg tb_expected_store_rx_packet_data;
-reg [7:0] tb_expected_rx_packet_data;
+logic [2:0] expected_rx_packet;
+logic expected_rx_data_ready;
+logic expected_rx_transfer_active;
+logic expected_rx_error;
+logic expected_flush;
+logic expected_store_rx_packet_data;
+logic [7:0] expected_rx_packet_data;
 
 // Test Bench Debug Signals
-reg [7:0] tb_current_usb_byte;
-reg tb_stuff_bit;
-reg [15:0] tb_packet;
-reg [4:0] tb_crc;
-reg [10:0] tb_data;
+reg [7:0] current_usb_byte;
 
 // Test Bench Verification Signals
-integer tb_test_num;
-string tb_test_case;
+int test_num;
+string test_case;
 
 // Task for Resetting DUT
 task reset_dut();
     // Activate reset signal
-    tb_n_rst = 1'b0;
+    n_rst = 1'b0;
 
     // Wait 2 clock cycles
-    @(posedge tb_clk);
-    @(posedge tb_clk);
+    @(posedge clk);
+    @(posedge clk);
 
     // Release reset signal away from clock's posedge
-    @(negedge tb_clk);
-    tb_n_rst = 1'b1;
+    @(negedge clk);
+    n_rst = 1'b1;
 
     // Wait
-    @(negedge tb_clk);
+    @(negedge clk);
 endtask
 
-`define info(mesg) $info("Test case %02d: %s", tb_test_num, mesg)
+`define info(mesg) $info("Test case %02d: %s", test_num, mesg)
 `define error(mesg, got, exp) $error( \
         "Test case %02d: %s (got %d, expected %d)", \
-        tb_test_num, mesg, got, exp \
+        test_num, mesg, got, exp \
     )
 
 task check_outputs(string check);
-    /* reg [2:0] tb_rx_packet; */
-    assert(tb_rx_packet == tb_expected_rx_packet)
+    /* reg [2:0] rx_packet; */
+    assert(rx_packet == expected_rx_packet)
         `info("correct rx_packet");
     else
-        `error("incorrect rx_packet", tb_rx_packet, tb_expected_rx_packet);
-    /* reg tb_rx_data_ready; */
-    assert(tb_rx_data_ready == tb_expected_rx_data_ready)
+        `error("incorrect rx_packet", rx_packet, expected_rx_packet);
+    /* reg rx_data_ready; */
+    assert(rx_data_ready == expected_rx_data_ready)
         `info("correct rx_data_ready");
     else
-        `error("rx_data_ready", tb_rx_data_ready, tb_expected_rx_data_ready);
-    /* reg tb_rx_transfer_active */
-    assert(tb_rx_transfer_active == tb_expected_rx_transfer_active)
+        `error("rx_data_ready", rx_data_ready, expected_rx_data_ready);
+    /* reg rx_transfer_active */
+    assert(rx_transfer_active == expected_rx_transfer_active)
         `info("correct rx_transfer_active");
     else
         `error(
             "rx_transfer_active",
-            tb_rx_transfer_active, tb_expected_rx_transfer_active
+            rx_transfer_active, expected_rx_transfer_active
         );
-    /* reg tb_rx_error; */
-    assert(tb_rx_error == tb_expected_rx_error)
+    /* reg rx_error; */
+    assert(rx_error == expected_rx_error)
         `info("correct rx_error");
     else
-        `error("rx_error", tb_rx_error, tb_expected_rx_error);
-    /* reg tb_flush; */
-    assert(tb_flush == tb_expected_flush)
+        `error("rx_error", rx_error, expected_rx_error);
+    /* reg flush; */
+    assert(flush == expected_flush)
         `info("correct flush");
     else
-        `error("flush", tb_flush, tb_expected_flush);
-    /* reg tb_store_rx_packet_data; */
-    assert(tb_store_rx_packet_data == tb_expected_store_rx_packet_data)
+        `error("flush", flush, expected_flush);
+    /* reg store_rx_packet_data; */
+    assert(store_rx_packet_data == expected_store_rx_packet_data)
         `info("correct store_rx_packet_data");
     else
         `error(
             "store_rx_packet_data",
-            tb_store_rx_packet_data, tb_expected_store_rx_packet_data
+            store_rx_packet_data, expected_store_rx_packet_data
         );
-    /* reg [7:0] tb_rx_packet_data; */
-    assert(tb_rx_packet_data == tb_expected_rx_packet_data)
+    /* reg [7:0] rx_packet_data; */
+    assert(rx_packet_data == expected_rx_packet_data)
         `info("correct rx_packet_data");
     else
-        `error("rx_packet_data", tb_rx_packet_data, tb_expected_rx_packet_data);
+        `error("rx_packet_data", rx_packet_data, expected_rx_packet_data);
 endtask
 
 typedef union tagged {
@@ -141,14 +140,14 @@ task send_usb_bit(input time period, input bit b, input bit eop = 1'b0);
     static int ones = 0;
 
     if (eop == 1'b1) begin
-        tb_dp = 1'b0;
-        tb_dm = 1'b0;
+        dp = 1'b0;
+        dm = 1'b0;
         #(period);
         return;
     end
 
-    tb_dp ^= ~b;
-    tb_dm = ~tb_dp;
+    dp ^= ~b;
+    dm = ~dp;
     #(period);
 
     if (b == 1'b1)
@@ -158,9 +157,7 @@ task send_usb_bit(input time period, input bit b, input bit eop = 1'b0);
 
     if (ones == 6) begin
         ones = 0;
-        tb_stuff_bit = 1'b1;
         send_usb_bit(period, 1'b0);
-        tb_stuff_bit = 1'b0;
     end
 endtask
 
@@ -172,7 +169,7 @@ task send_usb_packet(input time period = (250/3) * 1ns);
         usb_data = dequeue_usb_byte();
         case (usb_data) matches
             tagged usb_data_byte .b: begin
-                tb_current_usb_byte = b;
+                current_usb_byte = b;
                 for (int i = 0; i < 8; i++) begin
                     send_usb_bit(period, b[i]);
                 end
@@ -197,11 +194,8 @@ task enqueue_usb_packet(
     automatic byte pid_byte = {~pid, pid};
     enqueue_usb_byte(tagged usb_data_byte (8'h80));
     enqueue_usb_byte(tagged usb_data_byte (pid_byte));
-    $info("num: %d", n_bytes);
-    for (int i = 0; i < n_bytes; i++) begin
-        $info("b: %02x", bytes[i]);
+    for (int i = 0; i < n_bytes; i++)
         enqueue_usb_byte(tagged usb_data_byte (bytes[i]));
-    end
     enqueue_usb_byte(tagged usb_data_eop ('{8'h03, 8'h00, 3}));
 endtask
 
@@ -217,16 +211,13 @@ task enqueue_usb_token(
     automatic bit [10:0] data = {endpoint, address};
     automatic bit [4:0] crc = 5'b11111;
     logic xr;
-    tb_data = data;
     for (int i = 0; i < 11; i++) begin
         xr = data[i] ^ crc[4];
         crc = {crc[3:2], crc[1] ^ xr, crc[0], xr};
     end
 
     // send data
-    tb_crc = crc;
     bytes = { << byte { {{<<{~crc}}, endpoint[3:1]}, {endpoint[0], address} }};
-    tb_packet = {bytes[1], bytes[0]};
     enqueue_usb_packet(pid, 2, bytes);
 endtask
 
@@ -243,8 +234,6 @@ task enqueue_usb_data(input bit d_type, input int n_bytes, input byte data[]);
         end
     end
 
-    // byte data [0:3];
-    /* enqueue_usb_packet(pid, n_bytes + 2, {crc[15:8], crc[7:0], { << byte {data} }}); */
     enqueue_usb_packet(
         pid, n_bytes + 2,
         { data, { << {~crc[15:8]} }, { << {~crc[7:0]} } }
@@ -258,60 +247,47 @@ endtask
 
 // Clock Gen Block
 always begin: CLK_GEN
-    tb_clk = 1'b0;
+    clk = 1'b0;
     #(CLK_PERIOD / 2.0);
-    tb_clk = 1'b1;
+    clk = 1'b1;
     #(CLK_PERIOD / 2.0);
 end
 
 // DUT Port Map
-usb_rx DUT (
-    .clk(tb_clk), .n_rst(tb_n_rst),
-    // USB input lines
-    .dp(tb_dp), .dm(tb_dm),
-    // Outputs to AHB-lite interface
-    .rx_packet(tb_rx_packet),
-    .rx_data_ready(tb_rx_data_ready),
-    .rx_transfer_active(tb_rx_transfer_active),
-    .rx_error(tb_rx_error),
-    // I/O from FIFO
-    .flush(tb_flush), .store_rx_packet_data(tb_store_rx_packet_data),
-    .rx_packet_data(tb_rx_packet_data),
-    .buffer_occupancy(tb_buffer_occupancy)
-);
+usb_rx DUT (.*);
 
 // Test Bench Main Process
 initial
 begin
     // Initialize all test bench signals
-    tb_test_num = 0;
-    tb_test_case = "TB Init";
+    test_num = 0;
+    test_case = "TB Init";
 
-    tb_n_rst = 1'b1;
+    n_rst = 1'b1;
 
-    tb_dp = 1'b1;
-    tb_dm = 1'b0;
-    tb_buffer_occupancy = 7'd0;
+    dp = 1'b1;
+    dm = 1'b0;
+    buffer_occupancy = 7'd0;
 
-    tb_expected_rx_packet = 3'd0;
-    tb_expected_rx_data_ready = 1'b0;
-    tb_expected_rx_transfer_active = 1'b0;
-    tb_expected_rx_error = 1'b0;
-    tb_expected_flush = 1'b0;
-    tb_expected_store_rx_packet_data = 1'b0;
-    tb_expected_rx_packet_data = 1'b0;
+    expected_rx_packet = 3'd0;
+    expected_rx_data_ready = 1'b0;
+    expected_rx_transfer_active = 1'b0;
+    expected_rx_error = 1'b0;
+    expected_flush = 1'b0;
+    expected_store_rx_packet_data = 1'b0;
+    expected_rx_packet_data = 1'b0;
 
     #(0.1);
 
     // **************************************************
     // Test Case 1: Power-on Reset of DUT
     // **************************************************
-    tb_test_num = tb_test_num + 1;
-    tb_test_case = "Power-on Reset of DUT";
+    test_num = test_num + 1;
+    test_case = "Power-on Reset of DUT";
 
-    @(negedge tb_clk);
+    @(negedge clk);
     #(0.1);
-    tb_n_rst = 1'b0;
+    n_rst = 1'b0;
 
     // Check internal state was correctly reset
     #(CLK_PERIOD * 0.5);
@@ -322,8 +298,8 @@ begin
     check_outputs("after clock cycle while in reset");
 
     // Release reset away from a clock edge
-    @(negedge tb_clk);
-    tb_n_rst = 1'b1;
+    @(negedge clk);
+    n_rst = 1'b1;
     #(CLK_PERIOD * 2);
 
     // Check internal state is maintained after reset released
@@ -332,8 +308,8 @@ begin
     // **************************************************
     // Test Case 2: Power-on Reset of DUT
     // **************************************************
-    tb_test_num = tb_test_num + 1;
-    tb_test_case = "Power-on Reset of DUT";
+    test_num = test_num + 1;
+    test_case = "Power-on Reset of DUT";
 
     reset_dut();
 
@@ -345,6 +321,8 @@ begin
 
     enqueue_usb_handshake(2'd0);
     send_usb_packet();
+
+    // TODO write more test cases
 
 end
 
