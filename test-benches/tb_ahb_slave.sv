f9c5589531eec826e954ea342c2db987bc5f7e19
Bryce Berger <bryce.z.berger@gmail.com>
2022-12-07 09:29:25 -0500

    Finish ahb_slave testbench, works mapped

--- test-benches/tb_ahb_slave.sv
+++ test-benches/tb_ahb_slave.sv
@@ -1,4 +1,5 @@
 `default_nettype none `timescale 1ns / 10ps
+`include "source/states.sv"
 
 class address_gen;
     bit write = 0;
@@ -31,17 +32,161 @@ class address_gen;
     }
 
     constraint normal_register {addr1 inside {['h4 : 'hf]};}
+
+    constraint write_to_cd {
+        (addr1 == 'hc) -> data[31:16] == 0;
+        (addr1 == 'hc) -> data[7] == 1;
+        (addr1 == 'hd) -> data[31:8] == 0;
+    }
+
+    constraint data_size {
+        (size == 0) -> data[31:7] == 0;
+        (size == 1) -> data[31:16] == 0;
+    }
+endclass
+
+class ahb_bus;
+    typedef struct {
+        logic [31:0] data;
+        logic [3:0] addr;
+        logic [1:0] size;
+        logic err, write, sel;
+    } ahb_packet;
+
+    ahb_packet q[$];
+
+    static logic clk;
+
+    // ahb inputs
+    static logic hsel = 0;
+    static logic [3:0] haddr = 0;
+    static logic [1:0] hsize = 0, htrans = 0;
+    static logic hwrite = 0;
+    static logic [31:0] hwdata = 0;
+    // ahb outputs
+    static logic [31:0] hrdata;
+    static logic hresp, hready;
+
+    function void add(input logic [31:0] data, input logic [3:0] addr,
+                      logic [1:0] size = 2, logic err = 0, logic write = 0,
+                      logic sel = 1);
+        this.q.push_back('{data: data, addr: addr, size: size, err: err,
+                         write: write, sel: sel});
+    endfunction
+
+    task sendall();
+        this.execute(this.q.size());
+    endtask
+
+    task execute(input int num = 1);
+        ahb_packet prev;
+        ahb_packet cur;
+
+        if (num > this.q.size()) num = this.q.size();
+        if (num < 1) return;
+
+        prev = this.q.pop_front();
+
+        @(negedge this.clk);
+        this.set_outputs_address(prev);
+        this.htrans = BUSY;
+        @(posedge this.clk);
+
+        for (int i = 1; i < num; i++) begin
+            @(negedge this.clk);
+            this.check_err(prev);
+            cur = this.q.pop_front();
+            this.set_outputs_address(cur);
+            this.set_outputs_data(prev);
+
+            @(posedge this.clk);
+            while (!hready) @(posedge this.clk);
+
+            #(0.1);
+            this.check(prev);
+            prev = cur;
+        end
+
+        // last one still needs to go through data phase
+        @(negedge this.clk);
+        this.set_outputs_data(prev);
+        @(posedge this.clk);
+        while (!hready) @(posedge this.clk);
+
+        @(negedge this.clk);
+        this.check(prev);
+        this.reset();
+    endtask
+
+    function void set_outputs_address(input ahb_packet pak);
+        this.hsel   = pak.sel;
+        this.haddr  = pak.addr;
+        this.hsize  = pak.size;
+        this.hwrite = pak.write;
+    endfunction
+
+    function void set_outputs_data(input ahb_packet pak);
+        if (pak.write) this.hwdata = pak.data;
+    endfunction
+
+    function reset();
+        this.hsel   = 0;
+        this.haddr  = 0;
+        this.hsize  = 0;
+        this.hwrite = 0;
+        this.hwdata = 0;
+        this.htrans = IDLE;
+    endfunction
+
+    function void check(input ahb_packet pak);
+        if (!pak.sel) begin
+            assert (this.hrdata == 0)
+            else
+                $error(
+                    "Incorrect hrdata response at time %t (expected 0x00000000, got 0x%08x)",
+                    $time,
+                    this.hrdata
+                );
+            return;
+        end
+
+        if (pak.write == 0) begin
+            assert (this.hrdata == pak.data)
+            else
+                $error(
+                    "Incorrect hrdata response at time %t (expected 0x%08x, got 0x%08x)",
+                    $time,
+                    pak.data,
+                    this.hrdata
+                );
+        end
+    endfunction
+
+    function void check_err(input ahb_packet pak);
+        if (!pak.sel)
+            assert (this.hresp == 0)
+            else
+                $error(
+                    "Incorrect hresp response at time %t (expected 0, got %b)",
+                    $time,
+                    this.hresp
+                );
+        else
+            assert (this.hresp == pak.err)
+            else
+                $error(
+                    "Incorrect hresp response at time %t (expected %b, got %b)",
+                    $time,
+                    pak.err,
+                    this.hresp
+                );
+    endfunction
 endclass
 
 module tb_ahb_slave ();
     // timing constants
     localparam time CLK_PERIOD = 10ns;
     // Sizing related constants
-    localparam int DATA_WIDTH = 2;
-    localparam int ADDR_WIDTH = 4;
-    localparam int DATA_WIDTH_BITS = DATA_WIDTH * 8;
-    localparam int DMB = DATA_WIDTH_BITS - 1;
-    localparam int AMB = ADDR_WIDTH - 1;
     // general constants
     localparam bit VERBOSE = 0;
 
@@ -49,24 +194,6 @@ module tb_ahb_slave ();
     int test_num;
     string test_case;
     string subtest_case;
-
-    // expected outputs
-    logic [31:0] expected_hrdata;
-    logic expected_hresp, expected_hready;
-
-    // needed to control ahb bus model
-    logic
-        enqueue_transaction,
-        transaction_write,
-        transaction_fake,
-        transaction_error,
-        current_transaction_error,
-        enable_transactions,
-        model_reset;
-    logic [2:0] transaction_size;
-    logic [AMB:0] transaction_addr;
-    logic [DMB:0] transaction_data;
-    int current_transaction_num;
 
     // general inputs
     logic clk, n_rst;
@@ -98,9 +225,21 @@ module tb_ahb_slave ();
     logic [31:0] tx_data;
     logic clear_data_buffer;
 
-    ahb_slave DUT (.*);
+    address_gen rng;
+    ahb_bus bus = new;
 
-    ahb_lite_bus BFM (.*);
+    assign bus.clk    = clk;
+    assign hsel       = bus.hsel;
+    assign haddr      = bus.haddr;
+    assign htrans     = bus.htrans;
+    assign hsize      = bus.hsize;
+    assign hwrite     = bus.hwrite;
+    assign hwdata     = bus.hwdata;
+    assign bus.hrdata = hrdata;
+    assign bus.hresp  = hresp;
+    assign bus.hready = hready;
+
+    ahb_slave DUT (.*);
 
     task reset_dut();
         n_rst = 1'b0;
@@ -119,53 +258,6 @@ module tb_ahb_slave ();
         } = '0;
     endtask
 
-    /* default:
-     * for device
-     * read
-     * address 0
-     * data 0
-     * no error
-     * 16 bits
-     */
-    task queue(bit for_dut = 1, bit write = 0, bit [AMB:0] address = 0,
-               bit [DMB:0] data = 0, bit expected_error = 0,
-               bit [1:0] size = 2);
-        // Make sure enqueue flag is low (will need a 0->1 pulse later)
-        enqueue_transaction = 1'b0;
-
-        // Setup info about transaction
-        transaction_fake    = ~for_dut;
-        transaction_write   = write;
-        transaction_addr    = address;
-        transaction_data    = data;
-        transaction_error   = expected_error;
-        transaction_size    = {2'b00, size};
-
-        // Pulse the enqueue flag
-        #0.1ns;
-        enqueue_transaction = 1'b1;
-        #0.1ns;
-        enqueue_transaction = 1'b0;
-    endtask
-
-    task execute_transactions(int num_transactions = 1);
-        // Activate the bus model
-        enable_transactions = 1'b1;
-        @(posedge clk);
-
-        // Process the transactions (all but last one overlap 1 out of 2 cycles
-        for (int wait_var = 0; wait_var < num_transactions; wait_var++) begin
-            @(posedge clk);
-        end
-
-        // Run out the last one (currently in data phase)
-        @(posedge clk);
-
-        // Turn off the bus model
-        @(negedge clk);
-        enable_transactions = 1'b0;
-    endtask
-
     task new_test(string name = "");
         @(negedge clk);
         @(negedge clk);
@@ -173,8 +265,6 @@ module tb_ahb_slave ();
         test_case    = name;
         subtest_case = "";
         test_num     = test_num + 1;
-        init_fir_side();
-        init_expected_outs();
     endtask
 
     /* svlint off legacy_always */
@@ -189,7 +279,8 @@ module tb_ahb_slave ();
     /* svlint on legacy_always */
 
     initial begin
-        address_gen rng = new;
+        $timeformat(-9, 2, " ns", 20);
+        rng = new;
         rng.invalid_write.constraint_mode(0);
 
         // Initialize Test Case Navigation Signals
@@ -209,8 +300,8 @@ module tb_ahb_slave ();
 
         // all outputs should be initialized to 0
         assert ({
-            d_mode, hrdata, hresp, hready,
-            tx_packet, tx_start, get_rx_data, store_tx_data, tx_data,
+            d_mode, hrdata, hresp,
+            tx_start, get_rx_data, store_tx_data,
             clear_data_buffer
         } == '0);
 
@@ -218,63 +309,71 @@ module tb_ahb_slave ();
         // Basic Write / Read
         // **************************************************
         new_test("Isolated single read / write");
+        buffer_occupancy   = 1;
+        tx_transfer_active = 1;
         reset_dut();
+
 
         rng.write    = 1;
 
         subtest_case = "4 byte";
         rng.size     = 2;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(2));
-        execute_transactions(1);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(2));
+        bus.execute(1);
         @(negedge clk);
-        queue(.address(rng.addr2), .data(rng.data), .write(0), .size(2));
-        execute_transactions(1);
+        bus.add(.addr(rng.addr2), .data(rng.data), .write(0), .size(2));
+        bus.execute(1);
 
         @(negedge clk);
         subtest_case = "2 byte";
         rng.size     = 1;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(1));
-        execute_transactions(1);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(1));
+        bus.execute(1);
         @(negedge clk);
-        queue(.address(rng.addr2), .data(rng.data), .write(0), .size(1));
-        execute_transactions(1);
+        bus.add(.addr(rng.addr2), .data(rng.data), .write(0), .size(1));
+        bus.execute(1);
 
         @(negedge clk);
         subtest_case = "1 byte";
         rng.size     = 0;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(0));
-        execute_transactions(1);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(0));
+        bus.execute(1);
         @(negedge clk);
-        queue(.address(rng.addr2), .data(rng.data), .write(0), .size(0));
-        execute_transactions(1);
+        bus.add(.addr(rng.addr2), .data(rng.data), .write(0), .size(0));
+        bus.execute(1);
 
         new_test("Overlapping single read / write");
 
         subtest_case = "4 byte";
         rng.size     = 2;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(2));
-        queue(.address(rng.addr2), .data(rng.data), .write(0), .size(2));
-        execute_transactions(2);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(2));
+        bus.add(.addr(rng.addr2), .data(rng.data), .write(0), .size(2));
+        bus.execute(2);
 
         @(negedge clk);
         subtest_case = "2 byte";
         rng.size     = 1;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(1));
-        queue(.address(rng.addr2), .data(rng.data), .write(0), .size(1));
-        execute_transactions(2);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(1));
+        bus.add(.addr(rng.addr2), .data(rng.data), .write(0), .size(1));
+        bus.execute(2);
 
         @(negedge clk);
         subtest_case = "1 byte";
         rng.size     = 0;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(0));
-        queue(.address(rng.addr2), .data(rng.data), .write(0), .size(0));
-        execute_transactions(2);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(0));
+        bus.add(.addr(rng.addr2), .data(rng.data), .write(0), .size(0));
+        bus.execute(2);
+
+        rng.write_to_cd.constraint_mode(0);
+
+        buffer_occupancy   = 0;
+        tx_transfer_active = 0;
 
         // **************************************************
         // Bad Write
@@ -288,33 +387,32 @@ module tb_ahb_slave ();
         subtest_case = "4 byte";
         rng.size     = 2;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(2),
-              .err(1));
-        queue(.address(rng.addr2), .data(0), .write(0), .size(2));
-        execute_transactions(2);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(2),
+                .err(1));
+        bus.add(.addr(rng.addr2), .data(0), .write(0), .size(2));
+        bus.execute(2);
 
         subtest_case = "2 byte";
         rng.size     = 1;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(1),
-              .err(1));
-        queue(.address(rng.addr2), .data(0), .write(0), .size(1));
-        execute_transactions(2);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(1),
+                .err(1));
+        bus.add(.addr(rng.addr2), .data(0), .write(0), .size(1));
+        bus.execute(2);
 
         subtest_case = "1 byte";
         rng.size     = 0;
         assert (rng.randomize() == 1);
-        queue(.address(rng.addr1), .data(rng.data), .write(1), .size(0),
-              .err(1));
-        queue(.address(rng.addr2), .data(0), .write(0), .size(0));
-        execute_transactions(2);
+        bus.add(.addr(rng.addr1), .data(rng.data), .write(1), .size(0),
+                .err(1));
+        bus.add(.addr(rng.addr2), .data(0), .write(0), .size(0));
+        bus.execute(2);
 
         rng.invalid_write.constraint_mode(0);
         rng.valid_write.constraint_mode(1);
 
         // wait out a few clock cycles before the final disable
         for (int i = 0; i < 20; i++) @(posedge clk);
+        // disable CLK_GEN;
     end
-
-    final disable CLK_GEN;
 endmodule
